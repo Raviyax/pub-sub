88f3bd431945c714535a538adab4c79d8b471558
raviyax <2021is043@stu.ucsc.cmb.ac.lk>
2024-08-04 02:11:42 +0530

    fix

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 
 #define BUFFER_SIZE 1024
 
-void error_handling(char *message) {
+void error_handling(const char *message) {
     perror(message);
     exit(1);
 }
@@ -18,7 +18,7 @@ int main(int argc, char *argv[]) {
     int str_len;
 
     if (argc != 3) {
-        printf("Usage: %s <IP> <port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <IP> <port>\n", argv[0]);
         exit(1);
     }
 
@@ -30,22 +30,38 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
+    if (serv_addr.sin_addr.s_addr == INADDR_NONE) {
+        close(sock);
+        error_handling("inet_addr() error");
+    }
 
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
+        close(sock);
         error_handling("connect() error");
+    }
 
     while (1) {
-        printf("Input message(Q to quit): ");
-        fgets(message, BUFFER_SIZE, stdin);
-
-        if (!strcmp(message, "terminate\n")) {
-            write(sock, message, strlen(message));
-            break;
+        printf("Input message (terminate to quit): ");
+        if (fgets(message, BUFFER_SIZE, stdin) == NULL) {
+            close(sock);
+            error_handling("fgets() error");
         }
 
-        write(sock, message, strlen(message));
+        if (write(sock, message, strlen(message)) == -1) {
+            close(sock);
+            error_handling("write() error");
+        }
+
+        if (!strcmp(message, "/terminate\n"))
+            break;
+
         str_len = read(sock, message, BUFFER_SIZE - 1);
-        message[str_len] = 0;
+        if (str_len == -1) {
+            close(sock);
+            error_handling("read() error");
+        }
+
+        message[str_len] = '\0';
         printf("Message from server: %s\n", message);
     }
 
--- src/server.c
+++ src/server.c
@@ -11,14 +11,48 @@ void error_handling(char *message) {
     exit(1);
 }
 
+void handle_client(int clnt_sock) {
+    char message[BUFFER_SIZE];
+    int str_len;
+
+    while (1) {
+        str_len = read(clnt_sock, message, sizeof(message) - 1);
+        if (str_len == -1) {
+            perror("read() error");
+            close(clnt_sock);
+            return;
+        }
+
+        if (str_len == 0) {
+            printf("Client disconnected.\n");
+            close(clnt_sock);
+            return;
+        }
+
+        message[str_len] = '\0';
+        printf("Message from client: %s\n", message);
+
+        if (strcmp(message, "terminate\n") == 0) {
+            printf("Client requested termination.\n");
+            close(clnt_sock);
+            return;
+        }
+
+        if (write(clnt_sock, message, str_len) == -1) {
+            perror("write() error");
+            close(clnt_sock);
+            return;
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     int serv_sock, clnt_sock;
     struct sockaddr_in serv_addr, clnt_addr;
     socklen_t clnt_addr_size;
-    char message[BUFFER_SIZE];
 
     if (argc != 2) {
-        printf("Usage: %s <port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         exit(1);
     }
 
@@ -37,28 +71,19 @@ int main(int argc, char *argv[]) {
     if (listen(serv_sock, 5) == -1)
         error_handling("listen() error");
 
-    clnt_addr_size = sizeof(clnt_addr);
-    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-    if (clnt_sock == -1)
-        error_handling("accept() error");
-
     while (1) {
-        int str_len = read(clnt_sock, message, sizeof(message) - 1);
-        if (str_len == -1)
-            error_handling("read() error");
-
-        message[str_len] = '\0';
-        printf("Message from client: %s\n", message);
-
-        if (strcmp(message, "terminate\n") == 0) {
-            printf("Client disconnected.\n");
-            break;
+        clnt_addr_size = sizeof(clnt_addr);
+        clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+        if (clnt_sock == -1) {
+            perror("accept() error");
+            continue;
         }
-        
-        write(clnt_sock, message, str_len);
+
+        printf("Connected to client: %s:%d\n", inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+
+        handle_client(clnt_sock);
     }
 
-    close(clnt_sock);
     close(serv_sock);
     return 0;
 }
