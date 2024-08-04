d90c178e87b52a96d1d03db6636f54f3834fba32
iamtrazy <iamtrazy@proton.me>
2024-08-04 17:26:26 +0530

    feat: add ip addresses to server log

--- src/server.c
+++ src/server.c
@@ -17,8 +17,7 @@ static _Atomic unsigned int cli_count = 0;
 static int uid = 10;
 
 /* Client structure */
-typedef struct
-{
+typedef struct {
     struct sockaddr_in address;
     int sockfd;
     int uid;
@@ -31,26 +30,21 @@ client_t *clients[MAX_CLIENTS];
 
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void str_overwrite_stdout()
-{
+void str_overwrite_stdout() {
     printf("\r%s", "> ");
     fflush(stdout);
 }
 
-void str_trim_lf(char *arr, int length)
-{
-    for (int i = 0; i < length; i++)
-    { // trim \n
-        if (arr[i] == '\n')
-        {
+void str_trim_lf(char *arr, int length) {
+    for (int i = 0; i < length; i++) { // trim \n
+        if (arr[i] == '\n') {
             arr[i] = '\0';
             break;
         }
     }
 }
 
-void print_client_addr(struct sockaddr_in addr)
-{
+void print_client_addr(struct sockaddr_in addr) {
     printf("%d.%d.%d.%d",
            addr.sin_addr.s_addr & 0xff,
            (addr.sin_addr.s_addr & 0xff00) >> 8,
@@ -59,14 +53,11 @@ void print_client_addr(struct sockaddr_in addr)
 }
 
 /* Add clients to queue */
-void queue_add(client_t *cl)
-{
+void queue_add(client_t *cl) {
     pthread_mutex_lock(&clients_mutex);
 
-    for (int i = 0; i < MAX_CLIENTS; ++i)
-    {
-        if (!clients[i])
-        {
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (!clients[i]) {
             clients[i] = cl;
             break;
         }
@@ -76,16 +67,12 @@ void queue_add(client_t *cl)
 }
 
 /* Remove clients from queue */
-void queue_remove(int uid)
-{
+void queue_remove(int uid) {
     pthread_mutex_lock(&clients_mutex);
 
-    for (int i = 0; i < MAX_CLIENTS; ++i)
-    {
-        if (clients[i])
-        {
-            if (clients[i]->uid == uid)
-            {
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i]) {
+            if (clients[i]->uid == uid) {
                 clients[i] = NULL;
                 break;
             }
@@ -96,18 +83,13 @@ void queue_remove(int uid)
 }
 
 /* Send message to all subscribers of a specific topic */
-void send_message_to_subscribers(char *s, int uid, char *topic)
-{
+void send_message_to_subscribers(char *s, int uid, char *topic) {
     pthread_mutex_lock(&clients_mutex);
 
-    for (int i = 0; i < MAX_CLIENTS; ++i)
-    {
-        if (clients[i])
-        {
-            if (clients[i]->uid != uid && !clients[i]->is_publisher && strcmp(clients[i]->topic, topic) == 0)
-            {
-                if (write(clients[i]->sockfd, s, strlen(s)) < 0)
-                {
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i]) {
+            if (clients[i]->uid != uid && !clients[i]->is_publisher && strcmp(clients[i]->topic, topic) == 0) {
+                if (write(clients[i]->sockfd, s, strlen(s)) < 0) {
                     perror("ERROR: write to descriptor failed");
                     break;
                 }
@@ -119,8 +101,7 @@ void send_message_to_subscribers(char *s, int uid, char *topic)
 }
 
 /* Handle all communication with the client */
-void *handle_client(void *arg)
-{
+void *handle_client(void *arg) {
     char buff_out[BUFFER_SZ];
     char name[32];
     char topic[32];
@@ -131,45 +112,39 @@ void *handle_client(void *arg)
 
     /* Receive role (Publisher/Subscriber) */
     char role[32];
-    if (recv(cli->sockfd, role, 32, 0) <= 0)
-    {
+    if (recv(cli->sockfd, role, 32, 0) <= 0) {
         printf("ERROR: couldn't receive role.\n");
         leave_flag = 1;
     }
     str_trim_lf(role, strlen(role));
-    if (strcmp(role, "PUBLISHER") == 0)
-    {
+    if (strcmp(role, "PUBLISHER") == 0) {
         cli->is_publisher = 1;
-    }
-    else
-    {
+    } else {
         cli->is_publisher = 0;
     }
 
     /* Receive name */
-    if (recv(cli->sockfd, name, 32, 0) <= 0 || strlen(name) < 2 || strlen(name) >= 32 - 1)
-    {
+    if (recv(cli->sockfd, name, 32, 0) <= 0 || strlen(name) < 2 || strlen(name) >= 32 - 1) {
         printf("Didn't enter the name.\n");
         leave_flag = 1;
-    }
-    else
-    {
+    } else {
         strcpy(cli->name, name);
     }
 
     /* Receive topic */
-    if (recv(cli->sockfd, topic, 32, 0) <= 0 || strlen(topic) < 1 || strlen(topic) >= 32 - 1)
-    {
+    if (recv(cli->sockfd, topic, 32, 0) <= 0 || strlen(topic) < 1 || strlen(topic) >= 32 - 1) {
         printf("Didn't enter the topic.\n");
         leave_flag = 1;
-    }
-    else
-    {
+    } else {
         strcpy(cli->topic, topic);
     }
 
-    if (!leave_flag)
-    {
+    if (!leave_flag) {
+        // Log client IP address
+        char client_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(cli->address.sin_addr), client_ip, INET_ADDRSTRLEN);
+        printf("Client %s connected with IP address: %s\n", cli->name, client_ip);
+
         sprintf(buff_out, "%s has joined on topic %s\n", cli->name, cli->topic);
         printf("%s", buff_out);
         send_message_to_subscribers(buff_out, cli->uid, cli->topic);
@@ -177,42 +152,30 @@ void *handle_client(void *arg)
 
     bzero(buff_out, BUFFER_SZ);
 
-    while (1)
-    {
-        if (leave_flag)
-        {
+    while (1) {
+        if (leave_flag) {
             break;
         }
 
         int receive = recv(cli->sockfd, buff_out, BUFFER_SZ, 0);
-        if (receive > 0)
-        {
-            if (strlen(buff_out) > 0)
-            {
-                if (strcmp(buff_out, "/terminate") == 0)
-                {
+        if (receive > 0) {
+            if (strlen(buff_out) > 0) {
+                if (strcmp(buff_out, "/terminate") == 0) {
                     leave_flag = 1;
-                }
-                else
-                {
-                    if (cli->is_publisher)
-                    {
+                } else {
+                    if (cli->is_publisher) {
                         send_message_to_subscribers(buff_out, cli->uid, cli->topic);
                     }
                     str_trim_lf(buff_out, strlen(buff_out));
                     printf("%s -> %s\n", buff_out, cli->name);
                 }
             }
-        }
-        else if (receive == 0 || strcmp(buff_out, "/terminate") == 0)
-        {
+        } else if (receive == 0 || strcmp(buff_out, "/terminate") == 0) {
             sprintf(buff_out, "%s has left\n", cli->name);
             printf("%s", buff_out);
             send_message_to_subscribers(buff_out, cli->uid, cli->topic);
             leave_flag = 1;
-        }
-        else
-        {
+        } else {
             printf("ERROR: -1\n");
             leave_flag = 1;
         }
@@ -230,10 +193,8 @@ void *handle_client(void *arg)
     return NULL;
 }
 
-int main(int argc, char **argv)
-{
-    if (argc != 2)
-    {
+int main(int argc, char **argv) {
+    if (argc != 2) {
         printf("Usage: %s <port>\n", argv[0]);
         return EXIT_FAILURE;
     }
@@ -255,36 +216,31 @@ int main(int argc, char **argv)
     /* Ignore pipe signals */
     signal(SIGPIPE, SIG_IGN);
 
-    if (setsockopt(listenfd, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), (char *)&option, sizeof(option)) < 0)
-    {
+    if (setsockopt(listenfd, SOL_SOCKET, (SO_REUSEADDR), (char *)&option, sizeof(option)) < 0) {
         perror("ERROR: setsockopt failed");
         return EXIT_FAILURE;
     }
 
     /* Bind */
-    if (bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if (bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("ERROR: Socket binding failed");
         return EXIT_FAILURE;
     }
 
     /* Listen */
-    if (listen(listenfd, 10) < 0)
-    {
+    if (listen(listenfd, 10) < 0) {
         perror("ERROR: Socket listening failed");
         return EXIT_FAILURE;
     }
 
     printf("=== WELCOME TO THE PUBSUB ROOM ===\n");
 
-    while (1)
-    {
+    while (1) {
         socklen_t clilen = sizeof(cli_addr);
         connfd = accept(listenfd, (struct sockaddr *)&cli_addr, &clilen);
 
         /* Check if max clients is reached */
-        if ((cli_count + 1) == MAX_CLIENTS)
-        {
+        if ((cli_count + 1) == MAX_CLIENTS) {
             printf("Max clients reached. Rejected: ");
             print_client_addr(cli_addr);
             printf(":%d\n", cli_addr.sin_port);
